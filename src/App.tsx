f7f36cf9c31784a69ef731eed53aa31728bd94da
Youssef Elfishawi <yousefsafwat367@gmail.com>
2026-03-24 18:21:56 +0200

    feat: implement ProductCard component with image and details

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import ProductCard from './components/ProductCard'
 
 function App() {
 
   return (
-    <div className='text-amber-50'>
-      Hello world!
+    <div  className='m-6 grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 gap-4'>
+      <ProductCard  />
+      <ProductCard  />
+      <ProductCard  />
+      <ProductCard  />
+
     </div>
   )
 }
