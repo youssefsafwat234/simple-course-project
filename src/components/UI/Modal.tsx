5a25487925244c8f2fb623a3bb8d0275e0da73a4
Youssef Elfishawi <yousefsafwat367@gmail.com>
2026-04-22 13:53:11 +0200

    feat: add backdrop blur effect to Modal component

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -14,8 +14,8 @@ interface IProps {
 export default function Modal({ isOpen, open, close, children, title }: IProps) {
     return (
         <>
-            <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close} __demoMode>
-                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <Dialog open={isOpen} as="div" className=" relative z-10 focus:outline-none" onClose={close} __demoMode>
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto  backdrop-blur-xs">
                     <div className="flex min-h-full items-center justify-center p-4">
                         <DialogPanel
                             transition
